e8f66af8e4bcc39c32662aa3e82c0eabce6638f9
zhaoran <2680131355@qq.com>
2017-02-28 13:51:18 +0800

    个人中心头像昵称显示及退出方法

--- app/src/main/java/cn/ucai/live/ui/activity/MyProfileFragment.java
+++ app/src/main/java/cn/ucai/live/ui/activity/MyProfileFragment.java
@@ -12,10 +12,15 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import cn.ucai.live.LiveHelper;
 import cn.ucai.live.R;
 import cn.ucai.live.data.model.LiveSettings;
+import cn.ucai.live.utils.MFGT;
+
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+import com.hyphenate.easeui.widget.EaseImageView;
 
 public class MyProfileFragment extends Fragment {
     Unbinder unbinder;
@@ -23,7 +28,10 @@ public class MyProfileFragment extends Fragment {
     //Spinner spinner;
     //@BindView(R.id.frame_rate)
     //TextView frameRateText;
-    @BindView(R.id.tv_username) TextView usernameView;
+    @BindView(R.id.tv_username)
+    TextView usernameView;
+    @BindView(R.id.iv_avatar)
+    EaseImageView userAvatar;
 
     LiveSettings liveSettings;
 
@@ -39,8 +47,10 @@ public class MyProfileFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        usernameView.setText(EMClient.getInstance().getCurrentUser());
+//        usernameView.setText(EMClient.getInstance().getCurrentUser());
 
+        EaseUserUtils.setAppUserAvatar(getContext(),EMClient.getInstance().getCurrentUser(),userAvatar);
+        EaseUserUtils.setAppUserNick(EMClient.getInstance().getCurrentUser(),usernameView);
 
         //liveSettings = new LiveSettings(getContext());
         //final String[] bitrateArr = getResources().getStringArray(R.array.bitrate_types);
@@ -65,11 +75,12 @@ public class MyProfileFragment extends Fragment {
     }
 
     @OnClick(R.id.btn_logout) void onLogout(){
-        EMClient.getInstance().logout(false, new EMCallBack() {
+        LiveHelper.getInstance().logout(false, new EMCallBack() {
             @Override
             public void onSuccess() {
                 getActivity().finish();
-                startActivity(new Intent(getActivity(), LoginActivity.class));
+                MFGT.gotoLoginCleanTask(getActivity());
+//                startActivity(new Intent(getActivity(), LoginActivity.class));
             }
 
             @Override
