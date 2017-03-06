33c7602c1890efcb3fe251bff688bfc402c5730c
zhaoRan <2680131355@qq.com>
2017-03-06 15:20:02 +0800

    账户余额的界面显示

--- app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
@@ -1,14 +1,30 @@
 package cn.ucai.live.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.live.R;
 
 public class ChangeActivity extends BaseActivity {
 
+    @BindView(R.id.tv_change_balance)
+    TextView mTvChangeBalance;
+    @BindView(R.id.target_layout)
+    LinearLayout mTargetLayout;
+    View loadingView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change);
+        ButterKnife.bind(this);
+        loadingView = LayoutInflater.from(ChangeActivity.this).inflate(R.layout.rp_loading, null);
+        mTargetLayout.addView(loadingView);
     }
 }
